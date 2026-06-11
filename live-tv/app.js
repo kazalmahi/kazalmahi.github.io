ee1eac6b9d7184c541e92eef4d5773fcfaa8d5a3
Kazal Mahi <64190785+kazalmahi@users.noreply.github.com>
2026-06-12 02:15:44 +0600

    Create app.js

---
+++ live-tv/app.js
@@ -0,0 +1,59 @@
+const video = document.getElementById("video");
+const loader = document.getElementById("loader");
+
+const stream =
+"http://103.151.61.12/T-Sports.kutta/video.m3u8";
+
+if(Hls.isSupported()){
+
+const hls = new Hls({
+enableWorker:true,
+lowLatencyMode:true,
+maxBufferLength:30,
+backBufferLength:90
+});
+
+hls.loadSource(stream);
+hls.attachMedia(video);
+
+hls.on(Hls.Events.MANIFEST_PARSED,()=>{
+
+loader.style.display="none";
+video.play();
+
+});
+
+hls.on(Hls.Events.ERROR,(event,data)=>{
+
+if(data.fatal){
+loader.style.display="none";
+console.log(data);
+}
+
+});
+
+}else if(video.canPlayType(
+'application/vnd.apple.mpegurl'
+)){
+
+video.src=stream;
+
+video.addEventListener(
+'loadedmetadata',
+()=>{
+loader.style.display="none";
+video.play();
+}
+);
+
+}
+
+document
+.getElementById("fullscreenBtn")
+.addEventListener("click",()=>{
+
+if(video.requestFullscreen){
+video.requestFullscreen();
+}
+
+});
